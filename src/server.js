e7be842cad6bb3862356afe17588339e70b3435c
harteh <harteh31@gmail.com>
2022-12-29 22:50:43 +0900

    room count sids.get(key)

--- src/server.js
+++ src/server.js
@@ -15,6 +15,22 @@ app.get("/*", (req, res) => res.redirect("/"));
 const httpServer = http.createServer(app);  // http 서버
 const wsServer = new Server(httpServer);
 
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: { sids, rooms },
+        },
+    } = wsServer;
+    // console.log(wsServer.sockets.adapter);
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if (sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
 wsServer.on("connection", (socket) => {
     socket["nickname"] = "Anonymous";
     socket.onAny((event) => {
@@ -25,12 +41,16 @@ wsServer.on("connection", (socket) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
 
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => 
             socket.to(room).emit("bye", socket.nickname)
         );
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
     });
 
     socket.on("new_message", (msg, room, done) => {
